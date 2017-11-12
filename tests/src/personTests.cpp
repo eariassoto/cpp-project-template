c2d86f6171c41255d304ad59fb6d4a8588897a77
Emmanuel Arias <ariassotoemmanuel@gmail.com>
2017-11-12 16:06:57 +0000

    Add UT for invalid param in ctor

--- tests/src/personTests.cpp
+++ tests/src/personTests.cpp
@@ -1,5 +1,18 @@
 #include "Person.h"
+#include <stdexcept>
 #include <gtest/gtest.h>
+
+TEST(GreeterTests, PersonValidCtorTest) {
+	ASSERT_NO_THROW({
+		Person p{"Alexa"};
+	});
+}
+
+TEST(GreeterTests, PersonInvalidCtorTest) {
+	ASSERT_THROW({
+		Person p{""};
+	}, std::invalid_argument);
+}
 
 TEST(GreeterTests, PersonGetNameTest) {
   Person p{"Woz"};
